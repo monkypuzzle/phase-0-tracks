cf915dd6743d2d34683ec6c898c354247761d118
monkypuzzle <paul.newsam@gmail.com>
2017-03-07 17:16:54 -0800

    Create pseudocode for encrypt and decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,13 @@
+# Encrypt method:
+# - For each character in the string
+#   - If it's a letter, advance letter by one
+#   - Else if it's a space, ignore it
+# - Return the encrypted string
+
+# Decrypt method: 
+# - Define a variable that contains the entire alphabet
+#   - For each character in the string
+#     - Check its index within alphabet variable
+#     - Decrease index by 1 and redefine character in string as that decreased index
+# - Return decrypted string
+
