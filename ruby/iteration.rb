2a5a352439be79aa13f2c44bac25cbe33c5fe697
monkypuzzle <paul.newsam@gmail.com>
2017-03-13 16:30:52 -0700

    Adds different iterators for arrays and hashes.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+# Release 0
+
 def list_dinos()
   puts "Starting outside the block."
   puts "T-Rex"
@@ -8,6 +10,8 @@ def list_dinos()
 end
 
 list_dinos { |dino1, dino2, dino3| puts dino1; puts dino2; puts dino3 }
+
+# Release 1
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -32,3 +36,74 @@ numbers.map! do |x|
 end
 
 puts numbers
+
+# Release 2
+
+# 1.
+scores = [2, 3, 8, 15, 27]
+
+scores.delete_if { |score| score < 10 }
+
+puts scores
+
+hash = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5
+}
+
+puts hash.delete_if { |word, digit| digit < 3 }
+
+# 2.
+
+scores_two = [2, 3, 8, 15, 27]
+
+hash_two = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5
+}
+
+puts scores_two.select { |score| score < 10 }
+
+puts hash_two.select { |word, digit| digit < 3 }
+
+# 3. 
+
+scores_three = [2, 3, 8, 15, 27]
+
+hash_three = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5
+}
+
+puts scores_three.keep_if { |score| score < 10 } 
+
+puts hash_three.keep_if { |word, digit| digit < 3 }
+
+# 4.
+
+scores_four = [2, 3, 8, 15, 27]
+
+hash_four = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5
+}
+
+puts scores_four.take_while { |score| score < 10 }
+
+puts hash_four.take_while { |word, digit| digit < 3 }
+
+
+
+
