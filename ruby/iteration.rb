d36bbdc2aa7b627ee7513f9e98e724bf4bb6ad07
monkypuzzle <paul.newsam@gmail.com>
2017-03-13 16:05:52 -0700

    Adds each and map iterators

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -8,3 +8,27 @@ def list_dinos()
 end
 
 list_dinos { |dino1, dino2, dino3| puts dino1; puts dino2; puts dino3 }
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+hollywood_actors = {
+  "Brad Pitt" => "Interview With a Vampire",
+  "Ben Affleck" => "Argo",
+  "Matt Damon" => "Good Will Hunting",
+  "Angelina Jolie" => "Tomb Raider",
+  "Michael Cera" => "Superbad"
+}
+
+numbers.each do |x|
+  puts x
+end
+
+hollywood_actors.each do |actor, movie|
+  puts "#{actor} was the star of #{movie}."
+end
+
+numbers.map! do |x|
+  x * 10
+end
+
+puts numbers
