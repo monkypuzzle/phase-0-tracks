2d8d1bac32f4fbeac6d4dc77e731ea9933d649d5
monkypuzzle <paul.newsam@gmail.com>
2017-03-13 22:22:33 -0700

    Adds pseudocode, completing exercise.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,15 @@
 
+# Pseudocode
+# - Define a hash that contains all necessary data types.
+# - Prompt user for each datum, one at a time
+#   - Check entered datum is correctly formatted
+#   - Save each datum in corresponding key
+# - Print out hash for user's review
+# - Prompt user for corrections
+#   - If changes desired, ask user which key
+#   - Correct desired datum
+# - Print updated hash and exit program
+
 
 def generate_hash ()
   $client_details = {
