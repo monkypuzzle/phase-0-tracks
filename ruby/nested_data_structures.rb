2301540718d8d90f39974d5daaf276a780d9bed1
monkypuzzle <paul.newsam@gmail.com>
2017-03-14 08:35:08 -0700

    Print data using nested data structure calls

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,9 +1,21 @@
-drinks_menu {
+drinks_menu = {
   "Wine" => {
     "Red" => ['Beaujolais', 'Zinfandel', 'Tempranillo'],
     "White" => ['Chablis', 'Gewurztraminer', 'Riesling']
     },
-  "Beer" => ['IPA', 'Belgian White', 'Pilsner'],
+  "Beer" => [['West Coast IPA', 'American IPA'], 'Belgian White', 'Pilsner', 'Stout'],
   "Soda" => ['Coca-Cola', 'Seven-Up', 'Dr. Pepper'],
   "Water" => ['Still', 'Sparkling']
 }
+
+puts drinks_menu['Wine']['Red'][0]
+# Should print 'Beaujolais'
+
+puts drinks_menu['Beer'][0][0]
+# Should print 'West Coast IPA'
+
+puts drinks_menu['Water'][0]
+# Should print 'Still'
+
+print drinks_menu = drinks_menu['Wine']['White']
+# Should print ['Chablis', 'Gewurztraminer', 'Riesling']
