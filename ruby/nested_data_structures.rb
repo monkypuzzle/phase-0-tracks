eb92862a818d6e8ae21b2ceab0e5b9426be18309
monkypuzzle <paul.newsam@gmail.com>
2017-03-14 08:26:40 -0700

    Adds drinks_menu data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,9 @@
+drinks_menu {
+  "Wine" => {
+    "Red" => ['Beaujolais', 'Zinfandel', 'Tempranillo'],
+    "White" => ['Chablis', 'Gewurztraminer', 'Riesling']
+    },
+  "Beer" => ['IPA', 'Belgian White', 'Pilsner'],
+  "Soda" => ['Coca-Cola', 'Seven-Up', 'Dr. Pepper'],
+  "Water" => ['Still', 'Sparkling']
+}
