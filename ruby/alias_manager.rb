11d47a2e96b2e5515d1dd1768a681a2ea69ce799
monkypuzzle <paul.newsam@gmail.com>
2017-03-15 09:21:38 -0700

    Adds name_swap method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -30,15 +30,35 @@ def letter_increment (name)
   print letter_incremented_name.join('').split(' ').map!{|word|word.capitalize}.join(' ')
 end
 
-def create_alias ()
-  agent_alias = ''
-  puts 'What\'s your name, agent?'
-  agent_name = gets.chomp
-  agent_alias = name_swap(agent_name)
-  agent_alias = letter_increment(agent_name)
-  puts "Alright, #{agent_name}! You are no longer #{agent_name}. You are now: #{agent_alias}!"
+def name_swap (name)
+  old_name = name.split(' ')
+  new_name = []
+  old_name.each do |word|
+    if word == old_name.last
+      new_name << old_name[0]
+    else
+      new_name << old_name[(old_name.index(word) + 1)]
+    end
+  end
+  new_name.join(' ')
 end
+
+# def create_alias ()
+#   agent_alias = ''
+#   puts 'What\'s your name, agent?'
+#   agent_name = gets.chomp
+#   agent_alias = name_swap(agent_name)
+#   agent_alias = letter_increment(agent_name)
+#   puts "Alright, #{agent_name}! You are no longer #{agent_name}. You are now: #{agent_alias}!"
+# end
+
+# puts letter_increment('Richard Branson') 
+
+puts name_swap('Richard Branson')
 
 # create_alias
 
-puts letter_increment('Richard Branson') 
+
+
+
+
