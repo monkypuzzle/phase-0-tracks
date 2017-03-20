4d260e3b6af333939f7afe64fa819980484383da
monkypuzzle <paul.newsam@gmail.com>
2017-03-20 09:47:49 -0700

    Adds comments outlining class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+# BREED ------------------------
+# Mutt
+
+# CHARACTERISTICS ----------------
+# Ears: floppy
+# Friendly: absolutely
+# Eye color: brown
+# Name: Duncan
+# Coat color: piebald
+
+# BEHAVIOR -----------------------
+# Jump
+# Hug
+# Look cute
+
+
+
+class Puppy
+
+end
