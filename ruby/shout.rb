94f9e2afb94e350262f4bc4534cbf50ce6e2a49c
monkypuzzle <paul.newsam@gmail.com>
2017-03-22 09:22:18 -0700

    Adds second method to shout and calls method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -14,6 +14,11 @@ module Shout
   def yell_angrily(words)
     words.upcase + "!!!" + " :("
   end
+
+  def says_menacingly(words)
+    words.downcase + "... >:["
+  end
+
 end
 
 
@@ -32,5 +37,8 @@ blake = Blake_from_Mitch_and_Murray.new
 
 
 puts leonidas.yell_angrily("This is Sparta")
+puts leonidas.says_menacingly("Then we will fight in the shade")
+
 
 puts blake.yell_angrily("Always be closing")
+puts blake.says_menacingly("I'd wish you good luck but you wouldn't know what to do with it if you got it")
