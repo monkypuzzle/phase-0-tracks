3b045c2fe482d67d80a26bd7051321e84d3ecc98
monkypuzzle <paul.newsam@gmail.com>
2017-03-22 09:18:20 -0700

    Refactors Shout module as mixin, creates new shouting classes and calls method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,36 @@
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     "Hurray " + words.downcase + "! :)"
+#   end
+
+# end
+
 module Shout
-
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words.upcase + "!!!" + " :("
   end
-
-  def self.yelling_happily(words)
-    "Hurray " + words.downcase + "! :)"
-  end
-
 end
 
-puts Shout.yell_angrily("Hello you!")
 
-puts Shout.yelling_happily("Hello you!")
+class Leonidas
+  include Shout
+end
+
+class Blake_from_Mitch_and_Murray
+  include Shout
+end
+
+
+leonidas = Leonidas.new
+
+blake = Blake_from_Mitch_and_Murray.new
+
+
+puts leonidas.yell_angrily("This is Sparta")
+
+puts blake.yell_angrily("Always be closing")
