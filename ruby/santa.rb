2c14e68f29d77926b3c307a9d0eae6e15ae5c78f
monkypuzzle <paul.newsam@gmail.com>
2017-03-22 08:15:26 -0700

    Adds loop initializing santas, printout of intialized santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender,ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -12,4 +16,27 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
+  def print
+    puts "Gender is #{@gender}."
+    puts "Ethnicity is #{@ethnicity}."
+  end
+
+end
+
+# george = Santa.new
+# george.speak
+# george.eat_milk_and_cookies('gingersnap')
+
+santas = []
+
+genders = ["male","female","transgender"]
+ethnicities = ["black", "latino", "south asian", "pacific islander", "white"]
+
+ethnicities.each do | ethnicity |
+  santas << Santa.new(genders.sample, ethnicity)
+end
+
+ObjectSpace.each_object(Santa) do | santa |
+  p santa
+  
 end
