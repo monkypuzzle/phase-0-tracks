57d6c3588ad7c81b3e6eac28d8a7450af8a95182
monkypuzzle <paul.newsam@gmail.com>
2017-03-22 08:30:49 -0700

    Adds getter and setter methods for Santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -21,6 +21,27 @@ class Santa
     puts "Ethnicity is #{@ethnicity}."
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking.push(reindeer)
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
 end
 
 # george = Santa.new
@@ -38,5 +59,11 @@ end
 
 ObjectSpace.each_object(Santa) do | santa |
   p santa
-  
+  santa.gender = genders.sample
+  santa.celebrate_birthday
+  santa.get_mad_at("Dasher")
+  santa.age
+  santa.ethnicity
+  p santa
 end
+
